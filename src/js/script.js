491d7033d0cb0ec30556637249eb8eae39078b5e
unknown <arthurropke@hotmail.com>
2021-11-17 17:37:30 -0300

    menu select

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,11 @@
 import navMenu from './modules/navMenu.js'
+import menuAtivo from './modules/navAtivoPage.js'
 import typed from './modules/typed.js'
 import formMenu from './modules/form.js'
 import arrowToUp from './modules/arrowToUp.js'
 
 navMenu()
+menuAtivo()
 typed()
 formMenu()
 arrowToUp()
