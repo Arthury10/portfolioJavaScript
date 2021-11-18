aa917f679df10429771c1f1bcf61140ffd51225b
unknown <arthurropke@hotmail.com>
2021-11-18 09:25:15 -0300

    Corre

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,9 @@
 import navMenu from './modules/navMenu.js'
-import menuAtivo from './modules/navAtivoPage.js'
 import typed from './modules/typed.js'
 import formMenu from './modules/form.js'
 import arrowToUp from './modules/arrowToUp.js'
 
 navMenu()
-menuAtivo()
 typed()
 formMenu()
 arrowToUp()
